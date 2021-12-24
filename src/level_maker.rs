46b40e978a25e1ece1a4be7af03ca43967750393
Arkadiusz Piekarz <piekarzarkadiusz@gmail.com>
2021-12-24 21:46:11 +0100

    Add sequence 1 strands 4-7

--- src/level_maker.rs
+++ src/level_maker.rs
@@ -23,7 +23,7 @@ fn makeStrandInSequence1(strand: StrandNumber) -> Result<Level>
     let levelInfo = match strand.0 {
         1 => LevelInfo{
             start:  StrandInfo{numberOfNodes: 3, edges: vec![(0,1), (1,2)]},
-            target: StrandInfo{numberOfNodes: 3, edges: vec![(0,1), (0, 2)]},
+            target: StrandInfo{numberOfNodes: 3, edges: vec![(0,1), (0,2)]},
             maxMoves: 1},
         2 => LevelInfo{
             start:  StrandInfo{numberOfNodes: 5, edges: vec![(0,1), (1,2), (2,3), (1,4)]},
@@ -32,6 +32,22 @@ fn makeStrandInSequence1(strand: StrandNumber) -> Result<Level>
         3 => LevelInfo{
             start:  StrandInfo{numberOfNodes: 11, edges: vec![(0,1), (1,2), (1,3), (3,4), (3,5), (0,6), (6,7), (6,8), (8,9), (8,10)]},
             target: StrandInfo{numberOfNodes: 11, edges: vec![(0,1), (1,2), (2,3), (3,4), (3,5), (0,6), (6,7), (7,8), (8,9), (8,10)]},
+            maxMoves: 2},
+        4 => LevelInfo{
+            start:  StrandInfo{numberOfNodes: 11, edges: vec![(0,1), (1,2), (2,3), (2,4), (4,5), (1,6), (6,7), (7,8), (6,9), (0,10)]},
+            target: StrandInfo{numberOfNodes: 11, edges: vec![(0,1), (1,2), (2,3), (3,4), (2,5), (0,6), (6,7), (7,8), (7,9), (9,10)]},
+            maxMoves: 1},
+        5 => LevelInfo{
+            start:  StrandInfo{numberOfNodes: 10, edges: vec![(0,1), (1,2), (1,3), (0,4), (4,5), (4,6), (6,7), (7,8), (7,9)]},
+            target: StrandInfo{numberOfNodes: 10, edges: vec![(0,1), (1,2), (2,3), (3,4), (3,5), (1,6), (6,7), (7,8), (7,9)]},
+            maxMoves: 1},
+        6 => LevelInfo{
+            start:  StrandInfo{numberOfNodes: 13, edges: vec![(0,1), (1,2), (1,3), (3,4), (4,5), (4,6), (0,7), (7,8), (8,9), (9,10), (9,11), (7,12)]},
+            target: StrandInfo{numberOfNodes: 13, edges: vec![(0,1), (1,2), (2,3), (3,4), (3,5), (1,6), (0,7), (7,8), (7,9), (9,10), (10,11), (10,12)]},
+            maxMoves: 1},
+        7 => LevelInfo{
+            start:  StrandInfo{numberOfNodes: 13, edges: vec![(0,1), (1,2), (2,3), (3,4), (3,5), (5,6), (5,7), (2,8), (8,9), (9,10), (9,11), (8,12)]},
+            target: StrandInfo{numberOfNodes: 13, edges: vec![(0,1), (1,2), (2,3), (2,4), (4,5), (4,6), (0,7), (7,8), (8,9), (9,10), (9,11), (8,12)]},
             maxMoves: 2},
         _ => bail!("Unsupported strand number: {}", strand.0)
     };
