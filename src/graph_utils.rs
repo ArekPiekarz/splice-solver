64a197e925afd6701fad908012fe453e56341258
Arkadiusz Piekarz <piekarzarkadiusz@gmail.com>
2022-01-08 22:27:25 +0100

    Visualize doubler cells on images

--- src/graph_utils.rs
+++ src/graph_utils.rs
@@ -1,13 +1,21 @@
-use crate::strand::Strand;
+use crate::strand::{CellKind, NodeId, Strand};
 
 
-pub(crate) fn formatDotGraph(graph: &Strand) -> String
+pub(crate) fn formatDotGraph(strand: &Strand) -> String
 {
     let mut output = String::new();
     output.push_str("digraph {\n");
-    for edge in graph.collectEdges() {
-        output.push_str(&format!("    {} -> {}\n", edge.0, edge.1));
+    for edge in strand.collectEdges() {
+        output.push_str(&format!("    {} -> {}\n", formatNode(edge.0, strand), formatNode(edge.1, strand)));
     }
     output.push_str("}");
     output
+}
+
+fn formatNode(nodeId: NodeId, strand: &Strand) -> String
+{
+    match strand.cellKind(nodeId) {
+        CellKind::Normal => format!("{}", nodeId),
+        CellKind::Doubler => format!("\"{} (doubler)\"", nodeId)
+    }
 }
