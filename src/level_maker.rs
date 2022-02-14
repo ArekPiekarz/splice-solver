6f043f59a434ca1a67f86b04e52c0edc32429558
Arkadiusz Piekarz <piekarzarkadiusz@gmail.com>
2022-02-14 20:42:48 +0100

    Solve sequence 4 strand 3

--- src/level_maker.rs
+++ src/level_maker.rs
@@ -143,6 +143,10 @@ fn makeStrandInSequence4(strand: StrandNumber) -> Result<Level>
             start:  StrandInfo{nodeCount: 7, edges: vec![(0,1), (1,2), (1,3), (0,4), (4,5), (4,6)], mutables: vec![(2, Eraser)]},
             target: StrandInfo{nodeCount: 3, edges: vec![(0,1), (1,2)], mutables: vec![]},
             maxSplices: 1},
+        3 => LevelInfo{
+            start:  StrandInfo{nodeCount: 11, edges: vec![(0,1), (1,2), (1,3), (3,4), (3,5), (0,6), (6,7), (7,8), (7,9), (6,10)], mutables: vec![(5, Eraser)]},
+            target: StrandInfo{nodeCount: 9,  edges: vec![(0,1), (1,2), (2,3), (2,4), (0,5), (5,6), (6,7), (6,8)], mutables: vec![]},
+            maxSplices: 2}, // really 3, but it's angelic
         _ => bail!("Unsupported strand number: {}", strand.0)
     };
     Ok(makeLevelFrom(levelInfo))
